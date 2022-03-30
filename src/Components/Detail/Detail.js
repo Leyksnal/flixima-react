42b3838d012136b1e3f9fce75ab8d5896f3cc320
leyksnal <leyksnal@gmail.com>
2022-03-30 11:20:35 +0100

    min  changes

--- src/Components/Detail/Detail.js
+++ src/Components/Detail/Detail.js
@@ -5,7 +5,7 @@ export default function Detail() {
   return (
     <Container>
         <Bg>
-            <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/B409C2A425D58C32D822EB633C7CAE3DC910DC2FC62D2B1807A0BB092C531E9A/scale?width=1440&aspectRatio=1.78&format=jpeg" alt="" />
+            <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/2041CE26663867FC4EF20377B8625BD629E619452E23BCDB1AB259DD475C2EA1/scale?width=1440&aspectRatio=1.78" alt="" />
         </Bg>
         <h1>Hello</h1>
     </Container>
@@ -14,9 +14,13 @@ export default function Detail() {
 
 
 const Container = styled.div`
-    height: 100vh;
+    min-height: 100vh;
     width: 100%;
     position: relative;
+    background-image: url("https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/B409C2A425D58C32D822EB633C7CAE3DC910DC2FC62D2B1807A0BB092C531E9A/scale?width=1440&aspectRatio=1.78&format=jpeg");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
 `;
 
 const Bg = styled.div`
